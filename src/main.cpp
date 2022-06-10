023f7900deb3697135f5bbd26129d8914a2b3f92
Joel Cruz <joel_cruz410@yahoo.com>
2022-06-10 18:46:24 -0400

    ESP32 github initialize

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+#define WIFI_NETWORK "Hunter-Joel Wifi"
+#define WIFI_PASSWORD "hunterjoel"
+#define WIFI_TIMEOUT_MS 20000
+
+void connectToWiFi(){
+
+  Serial.print("Connecting to WiFi");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+
+  unsigned long startAttemptTime = millis();
+
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS)
+  {
+    Serial.print(".");
+    delay(100);
+  }
+
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("Failed!");
+    // take action
+  }
+  else
+  {
+    Serial.print("Connected!");
+    Serial.println(WiFi.localIP());
+  }
+}
+
+
+void setup() 
+{
+  Serial.begin(9600);  
+  connectToWiFi();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
