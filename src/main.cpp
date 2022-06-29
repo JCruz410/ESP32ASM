628d5e139688a1f37980ca037412798ab9c0b248
Joel Cruz <joel_cruz410@yahoo.com>
2022-06-28 21:31:31 -0400

    delay update

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include <ESPAsyncWebServer.h>
 
 
-const char* ssid = "Wifi name";
-const char* password = "Wifi Pass";
+const char* ssid = "wifi";
+const char* password = "pass";
 
 AsyncWebServer server(80);
 
@@ -27,16 +27,20 @@ void setup()
 // serial monitor prints ip address
  Serial.println(WiFi.localIP());
 
-// type ip address into web browser to confirm "hello world" print connection
+ // type ip address into web browser to confirm "hello world" print connection
  server.on("", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(200, "text/plain", "Hello World");
 
  });
 
  server.begin();
+
+
 }
 
 
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
